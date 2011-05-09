91afc69f1aac28066c06440aa348b3bf038c8a1b
Jonathan Boyett <jonathan@failingservers.com>
2011-05-09 16:23:50 -0700

    fix namespacing issues

--- lib/puppet/util/ipcidr.rb
+++ lib/puppet/util/ipcidr.rb
@@ -19,27 +19,31 @@
 
 require 'ipaddr'
 
-module Puppet::Util::IPCidr < IPAddr
+module Puppet
+  module Util
+    class IPCidr < IPAddr
   
-  def netmask
-    _to_string(@mask_addr)
+      def netmask
+        _to_string(@mask_addr)
+      end
+
+      def prefixlen
+        m = case @family
+            when Socket::AF_INET
+              IN4MASK
+            when Socket::AF_INET6
+              IN6MASK
+            else
+              raise "unsupported address family"
+            end
+        return $1.length if /\A(1*)(0*)\z/ =~ (@mask_addr & m).to_s(2)
+        raise "bad addr_mask format"
+      end
+
+      def cidr
+        cidr = sprintf("%s/%s", self.to_s, self.prefixlen)
+        cidr
+      end
+    end
   end
-
-   def prefixlen
-     m = case @family
-         when Socket::AF_INET
-           IN4MASK
-         when Socket::AF_INET6
-           IN6MASK
-         else
-           raise "unsupported address family"
-         end
-     return $1.length if /\A(1*)(0*)\z/ =~ (@mask_addr & m).to_s(2)
-     raise "bad addr_mask format"
-   end
-
-   def cidr
-     cidr = sprintf("%s/%s", self.to_s, self.prefixlen)
-     cidr
-   end
 end
