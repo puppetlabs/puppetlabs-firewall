c1d549fd1ef6b36850562faa479d825844e6b478
Jonathan Boyett <jonathan@failingservers.com>
2011-06-28 11:39:51 -0700

    create helper methods for provider, resource, and instance setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 require 'rspec'
 require 'puppet'
 $LOAD_PATH.unshift('../../lib', __FILE__)
+
+RSpec.configure do |config|
+  def setup_provider(type, prov)
+    @provider = Puppet::Type.type(type).provider(prov)
+  end
+
+  def setup_resource(type, options={})
+    @resource = Puppet::Type.type(type).new(options)
+  end
+
+  def setup_instance(prov, type)
+    @instance = prov.new(type)
+  end
+end
