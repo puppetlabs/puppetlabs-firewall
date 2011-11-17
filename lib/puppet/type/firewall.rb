4526c1eb19a7e436c1aa5de17be3ab8f195050d3
Jonathan Boyett <jonathan@failingservers.com>
2011-11-17 09:43:19 -0800

    (#10723) Munge hostnames and IPs to IPs with CIDR
    Previously when hostnames were used in the source and destination properties
    they were being converted to IP address by iptables. This meant that later
    comparisons were failing because the property in code (a hostname) and the
    'real' property returned by introspection (an ip address) were not matching.
    
    This code using the munge facility will automatically detect and convert
    hostnames to IP addresses in the type so the comparison works as expected.
    
    The side-effect is that puppet does the hostname to IP conversion, not
    iptables.

--- lib/puppet/type/firewall.rb
+++ lib/puppet/type/firewall.rb
@@ -87,6 +87,10 @@ Puppet::Type.newtype(:firewall) do
 
       The source can also be an IPv6 address if your provider supports it.
     EOS
+
+    munge do |value|
+      @resource.host_to_ip(value)
+    end
   end
 
   newproperty(:destination) do
@@ -97,6 +101,10 @@ Puppet::Type.newtype(:firewall) do
 
       The destination can also be an IPv6 address if your provider supports it.
     EOS
+
+    munge do |value|
+      @resource.host_to_ip(value)
+    end
   end
 
   newproperty(:sport, :array_matching => :all) do
