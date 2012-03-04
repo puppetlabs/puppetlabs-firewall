8ec25b4d4290e41a03100ab5865103f8f0c91163
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-03-04 19:33:58 +0100

    Note order flags should be listed in, for iptables

--- lib/puppet/type/firewall.rb
+++ lib/puppet/type/firewall.rb
@@ -215,7 +215,9 @@ Puppet::Type.newtype(:firewall) do
       Is a string with a list of comma-separated flag names for the mask,
       then a space, then a comma-separated list of flags that should be set.
       The flags are: SYN ACK FIN RST URG PSH ALL NONE
-      Example: SYN,RST,ACK,FIN SYN matches packets with the SYN bit set and the
+      Note that you specify them in the order that iptables --list-rules
+      would list them to avoid having puppet think you changed the flags.
+      Example: FIN,SYN,RST,ACK SYN matches packets with the SYN bit set and the
 	       ACK,RST and FIN bits cleared.  Such packets are used to request
                TCP  connection initiation.
     EOS
