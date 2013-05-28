d61ae15ff8276a03856557408a2754f367999d4e
Joe Julian <me@joejulian.name>
2013-05-28 13:47:33 -0700

    Swap log_prefix and log_level order to match the way it's saved
    Fixes #184
    Signed-off-by: Joe Julian <me@joejulian.name>

--- lib/puppet/provider/firewall/iptables.rb
+++ lib/puppet/provider/firewall/iptables.rb
@@ -87,7 +87,7 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
   # This order can be determined by going through iptables source code or just tweaking and trying manually
   @resource_list = [:table, :source, :destination, :iniface, :outiface,
     :proto, :isfragment, :tcp_flags, :gid, :uid, :sport, :dport, :port, :socket, :pkttype, :name, :state, :icmp, :limit, :burst,
-    :jump, :todest, :tosource, :toports, :log_level, :log_prefix, :reject, :set_mark]
+    :jump, :todest, :tosource, :toports, :log_prefix, :log_level, :reject, :set_mark]
 
   def insert
     debug 'Inserting rule %s' % resource[:name]
