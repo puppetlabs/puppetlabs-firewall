acbe1958e57519ce8d665d3f6fb7550926636a68
Ashley Penney <ashley.penney@puppetlabs.com>
2013-12-19 12:25:31 -0500

    Convert rspec-system tests to beaker-rspec.
    This work migrates the existing tests to beaker-rspec.

--- spec/acceptance/resource_cmd_spec.rb
+++ spec/acceptance/resource_cmd_spec.rb
@@ -1,53 +1,61 @@
-require 'spec_helper_system'
+require 'spec_helper_acceptance'
 
 # Here we want to test the the resource commands ability to work with different
 # existing ruleset scenarios. This will give the parsing capabilities of the
 # code a good work out.
 describe 'puppet resource firewall command:' do
   context 'make sure it returns no errors when executed on a clean machine' do
-    context puppet_resource('firewall') do
-      its(:exit_code) { should be_zero }
-      # don't check stdout, some boxes come with rules, that is normal
-      its(:stderr) { should be_empty }
+    it do
+      shell('puppet resource firewall') do |r|
+        r.exit_code.should be_zero
+        # don't check stdout, some boxes come with rules, that is normal
+        r.stderr.should be_empty
+      end
     end
   end
 
   context 'flush iptables and make sure it returns nothing afterwards' do
-    before :all do
+    before(:all) do
       iptables_flush_all_tables
     end
 
     # No rules, means no output thanks. And no errors as well.
-    context puppet_resource('firewall') do
-      its(:exit_code) { should be_zero }
-      its(:stderr) { should be_empty }
-      its(:stdout) { should == "\n" }
+    it do
+      shell('puppet resource firewall') do |r|
+        r.exit_code.should be_zero
+        r.stderr.should be_empty
+        r.stdout.should == "\n"
+      end
     end
   end
 
   context 'accepts rules without comments' do
-    before :all do
+    before(:all) do
       iptables_flush_all_tables
       shell('/sbin/iptables -A INPUT -j ACCEPT -p tcp --dport 80')
     end
 
-    context puppet_resource('firewall') do |r|
-      its(:exit_code) { should be_zero }
-      # don't check stdout, testing preexisting rules, output is normal
-      its(:stderr) { should be_empty }
+    it do
+      shell('puppet resource firewall') do |r|
+        r.exit_code.should be_zero
+        # don't check stdout, testing preexisting rules, output is normal
+        r.stderr.should be_empty
+      end
     end
   end
 
   context 'accepts rules with invalid comments' do
-    before :all do
+    before(:all) do
       iptables_flush_all_tables
       shell('/sbin/iptables -A INPUT -j ACCEPT -p tcp --dport 80 -m comment --comment "http"')
     end
 
-    context puppet_resource('firewall') do
-      its(:exit_code) { should be_zero }
-      # don't check stdout, testing preexisting rules, output is normal
-      its(:stderr) { should be_empty }
+    it do
+      shell('puppet resource firewall') do |r|
+        r.exit_code.should be_zero
+        # don't check stdout, testing preexisting rules, output is normal
+        r.stderr.should be_empty
+      end
     end
   end
 end
--- spec/acceptance/standard_usage_spec.rb
+++ spec/acceptance/standard_usage_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper_system'
+require 'spec_helper_acceptance'
 
 # Some tests for the standard recommended usage
 describe 'standard usage tests:' do
-  context 'standard 1' do
+  it 'applies twice' do
     pp = <<-EOS
       class my_fw::pre {
         Firewall {
@@ -48,12 +48,8 @@ describe 'standard usage tests:' do
       }
     EOS
 
-    context puppet_apply(pp) do
-      its(:stderr) { should be_empty }
-      its(:exit_code) { should_not == 1 }
-      its(:refresh) { should be_nil }
-      its(:stderr) { should be_empty }
-      its(:exit_code) { should be_zero }
-    end
+    # Run it twice and test for idempotency
+    apply_manifest(pp, :catch_failures => true)
+    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
   end
 end
