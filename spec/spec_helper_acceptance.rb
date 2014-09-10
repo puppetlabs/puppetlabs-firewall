1c7d88bde874db7390bf82b4bf9e5b9ec4a5cd34
Travis Fields <travis@puppetlabs.com>
2014-09-10 11:47:53 -0700

    Remove puppet_module_install in favor of copy_module_to

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -36,8 +36,8 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'firewall')
     hosts.each do |host|
+      copy_module_to(host, :source => proj_root, :module_name => 'firewall')
       shell('/bin/touch /etc/puppet/hiera.yaml')
       shell('puppet module install puppetlabs-stdlib --version 3.2.0', { :acceptable_exit_codes => [0,1] })
     end
