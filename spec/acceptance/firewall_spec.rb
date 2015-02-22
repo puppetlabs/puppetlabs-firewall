73ea8e7c592fed1811c57cc1e8de3e52d094df2c
Jonathan Davies <jpdavs@gmail.com>
2015-02-22 18:05:26 +0000

    set_mss_spec.rb: Moved into firewall_spec.rb instead.

--- spec/acceptance/firewall_spec.rb
+++ spec/acceptance/firewall_spec.rb
@@ -889,6 +889,33 @@ describe 'firewall type', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfami
     end
   end
 
+  describe 'set_mss' do
+    context '1360' do
+      it 'applies' do
+        pp = <<-EOS
+          class { '::firewall': }
+          firewall { 
+            '502 - set_mss':
+              proto   => 'tcp',
+              jump    => 'TCPMSS',
+              set_mss => '1360',
+              chain   => 'FORWARD',
+              table   => 'mangle',
+          }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+      end
+
+      it 'should contain the rule' do
+        shell('iptables-save') do |r|
+          expect(r.stdout).to match(/-A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -m comment --comment "502 - set_mss" -j TCPMSS --set-mss 1360/)
+        end
+      end
+    end
+  end
+
+
   # RHEL5 does not support --random
   if default['platform'] !~ /el-5/
     describe 'random' do
