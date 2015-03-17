d96a9c7aedf2b247982bd0cf33e601d9c0090a19
Chris Pisano <pisanoc@advisory.com>
2015-03-17 16:38:47 -0400

    stripped out default rules

--- manifests/post.pp
+++ manifests/post.pp
@@ -4,19 +4,4 @@ class firewall::post {
   Firewall {
     require => Class['firewall::pre'],
   }
-
-# default firewall rules
-
-  firewall { '999 reject everything':
-    proto  => 'all',
-    action => 'reject',
-    before => undef,
-  }
-
-  firewall { '998 reject everything':
-    proto  => 'all',
-    action => 'reject',
-    chain  => 'FORWARD',
-    before => undef,
-  }
 }
--- manifests/pre.pp
+++ manifests/pre.pp
@@ -6,30 +6,4 @@ class firewall::pre {
   Firewall {
     before => Class['firewall::post'],
   }
-
-# default firewall rules
-
-  firewall { '000 accept all icmp':
-    proto  => 'icmp',
-    action => 'accept',
-  }
-
-  firewall { '001 accept all to lo interface':
-    proto   => 'all',
-    action  => 'accept',
-    iniface => 'lo',
-  }
-
-  firewall { '002 accept related established rules':
-    proto  => 'all',
-    state  => ['RELATED', 'ESTABLISHED'],
-    action => 'accept',
-  }
-
-  firewall { '003 accept new ssh':
-    proto  => 'tcp',
-    state  => ['NEW'],
-    dport  => '22',
-    action => 'accept',
-  }
 }
