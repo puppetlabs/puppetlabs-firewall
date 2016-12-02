63e5ca8b8190466c90f28249bbbae32be9bd2126
Bryan Jen <bryan.jen@gmail.com>
2016-12-02 20:18:14 +0000

    Fixes SELinux compatibility with EL6

--- manifests/linux/redhat.pp
+++ manifests/linux/redhat.pp
@@ -76,12 +76,13 @@ class firewall::linux::redhat (
       #lint:ignore:quoted_booleans
       'true',true: {
         case $::operatingsystemrelease {
-          /^(6|7)\..*/: {
+          /^7\..*/: {
             case $::operatingsystem {
               'CentOS': { File["/etc/sysconfig/${service_name}"] { seluser => 'unconfined_u', seltype => 'system_conf_t' } }
               default : { File["/etc/sysconfig/${service_name}"] { seluser => 'unconfined_u', seltype => 'etc_t' } }
             }
           }
+          /^6\..*/:     { File["/etc/sysconfig/${service_name}"] { seluser => 'unconfined_u', seltype => 'system_conf_t' } }
           default:      { File["/etc/sysconfig/${service_name}"] { seluser => 'system_u', seltype => 'system_conf_t' } }
         }
       }
