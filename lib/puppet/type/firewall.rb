f36be9de1751c502583f12b05bab226e6ee36bef
Wilson McCoubrey <wilson@puppet.com>
2017-03-16 11:35:57 +0000

    [MODULES-4528] Replace Puppet.version.to_f with Puppet::Util::Package.versioncmp

--- lib/puppet/type/firewall.rb
+++ lib/puppet/type/firewall.rb
@@ -1555,7 +1555,7 @@ Puppet::Type.newtype(:firewall) do
   end
 
   # autobefore is only provided since puppet 4.0
-  if Puppet.version.to_f >= 4.0
+  if Puppet::Util::Package.versioncmp(Puppet.version, '4.0') >= 0
     # On RHEL 7 this needs to be threaded correctly to manage SE Linux permissions after persisting the rules
     autobefore(:file) do
       [ '/etc/sysconfig/iptables', '/etc/sysconfig/ip6tables' ]
