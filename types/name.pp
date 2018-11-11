fbb29d1a7037453a2a98b113e09b4a026f3591de
Bob Vincent <pillarsdotnet@gmail.com>
2018-11-10 23:10:09 -0500

    Bugfixes.

--- types/name.pp
+++ types/name.pp
@@ -1,2 +1,2 @@
 # The canonical name of the rule must start with a number.
-type Firewall::Name = Pattern(/\A\d+[[:graph:][:space:]]+\z/)
+type Firewall::Name = Pattern[/\A\d+[[:graph:][:space:]]+\z/]
--- types/ports.pp
+++ types/ports.pp
@@ -2,7 +2,7 @@
 # Due to pattern-match limitations this does not exclude repeated
 # values such as [1,1,1,1,1] or nonsense such as '100-99'.
 type Firewall::Ports = Variant[
-  Array[Firewall::Port,1,15]
+  Array[Firewall::Port,1,15],
   Firewall::Port,
   Pattern[/\A([1-9][0-9]{0,3}|[1-5][0-9]{4}|6([1-4][0-9]{3}|5([1-4][0-9]{2}|5([0-2][0-9]|3[0-5]))))-([1-9][0-9]{0,3}|[1-5][0-9]{4}|6([1-4][0-9]{3}|5([1-4][0-9]{2}|5([0-2][0-9]|3[0-5]))))\z/],
 ]
