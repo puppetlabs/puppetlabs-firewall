1648e3530776a0221da98aaec1f78dc7f1f05dbc
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-03-12 10:11:04 +0000

    Move bytecode tests

--- spec/acceptance/firewall_attributes_exceptions_spec.rb
+++ spec/acceptance/firewall_attributes_exceptions_spec.rb
@@ -6,6 +6,37 @@ describe 'firewall basics', docker: true do
     ip6tables_flush_all_tables
   end
 
+  # --bytecode is only supported by operatingsystems using nftables (in general Linux kernel 3.13, RedHat 7 (and derivates) with 3.10)
+  # Skipping those from which we know they would fail.
+  describe 'bytecode property', unless: (os[:family] == 'redhat' && os[:release][0] <= '6') ||
+                                        (os[:family] == 'sles' && os[:release][0..1] <= '11') ||
+                                        (host_inventory['facter']['os']['name'].casecmp('oraclelinux').zero? && os[:release][0] <= '7') do
+    describe 'bytecode' do
+      context '4,48 0 0 9,21 0 1 6,6 0 0 1,6 0 0 0' do
+        pp = <<-PUPPETCODE
+            class { '::firewall': }
+            firewall { '102 - test':
+              action   => 'accept',
+              bytecode => '4,48 0 0 9,21 0 1 6,6 0 0 1,6 0 0 0',
+              chain    => 'OUTPUT',
+              proto    => 'all',
+              table    => 'filter',
+            }
+        PUPPETCODE
+        it 'applies' do
+          apply_manifest(pp, catch_failures: true)
+        end
+
+        it 'contains the rule' do
+          shell('iptables-save') do |r|
+            expect(r.stdout).to match(%r{-A OUTPUT -m bpf --bytecode "4,48 0 0 9,21 0 1 6,6 0 0 1,6 0 0 0" -m comment --comment "102 - test" -j ACCEPT})
+          end
+        end
+      end
+    end
+  end
+
+
   describe 'name' do
     context 'when invalid ordering range specified' do
       pp = <<-PUPPETCODE
@@ -1186,11 +1217,6 @@ describe 'firewall basics', docker: true do
 
   # RHEL5 does not support -m socket
   describe 'socket', unless: (os[:family] == 'redhat' && os[:release].start_with?('5')) || os[:family] == 'sles' do
-    before :all do
-      iptables_flush_all_tables
-      ip6tables_flush_all_tables
-    end
-
     shared_examples 'is idempotent' do |value, line_match|
       pp1 = <<-PUPPETCODE
             class { '::firewall': }
