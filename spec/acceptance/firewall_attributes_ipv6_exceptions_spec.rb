080af79b12cad43544837959f2811208a10d3843
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-03-12 15:21:52 +0000

    Move ip6_fragment tests

--- spec/acceptance/firewall_attributes_ipv6_exceptions_spec.rb
+++ spec/acceptance/firewall_attributes_ipv6_exceptions_spec.rb
@@ -403,6 +403,96 @@ describe 'firewall ipv6 attribute testing, exceptions' do
     end
   end
 
+  describe 'ishasmorefrags/islastfrag/isfirstfrag' do
+    shared_examples 'is idempotent' do |values, line_match|
+      pp2 = <<-PUPPETCODE
+            class { '::firewall': }
+            firewall { '599 - test':
+              ensure   => present,
+              proto    => 'tcp',
+              provider => 'ip6tables',
+              #{values}
+            }
+      PUPPETCODE
+      it "changes the values to #{values}" do
+        apply_manifest(pp2, catch_failures: true)
+        apply_manifest(pp2, catch_changes: do_catch_changes)
+
+        shell('ip6tables-save') do |r|
+          expect(r.stdout).to match(%r{#{line_match}})
+        end
+      end
+    end
+    shared_examples "doesn't change" do |values, line_match|
+      pp3 = <<-PUPPETCODE
+            class { '::firewall': }
+            firewall { '599 - test':
+              ensure   => present,
+              proto    => 'tcp',
+              provider => 'ip6tables',
+              #{values}
+            }
+      PUPPETCODE
+      it "doesn't change the values to #{values}" do
+        apply_manifest(pp3, catch_changes: do_catch_changes)
+
+        shell('ip6tables-save') do |r|
+          expect(r.stdout).to match(%r{#{line_match}})
+        end
+      end
+    end
+
+    describe 'adding a rule' do
+      context 'when unset' do
+        before :all do
+          ip6tables_flush_all_tables
+        end
+        it_behaves_like 'is idempotent', '', %r{-A INPUT -p tcp -m comment --comment "599 - test"}
+      end
+      context 'when set to true' do
+        before :all do
+          ip6tables_flush_all_tables
+        end
+        it_behaves_like 'is idempotent', 'ishasmorefrags => true, islastfrag => true, isfirstfrag => true',
+                        %r{-A INPUT -p tcp -m frag --fragid 0 --fragmore -m frag --fragid 0 --fraglast -m frag --fragid 0 --fragfirst -m comment --comment "599 - test"}
+      end
+      context 'when set to false' do
+        before :all do
+          ip6tables_flush_all_tables
+        end
+        it_behaves_like 'is idempotent', 'ishasmorefrags => false, islastfrag => false, isfirstfrag => false', %r{-A INPUT -p tcp -m comment --comment "599 - test"}
+      end
+    end
+    describe 'editing a rule' do
+      context 'when unset or false' do
+        before :each do
+          ip6tables_flush_all_tables
+          shell('ip6tables -A INPUT -p tcp -m comment --comment "599 - test"')
+        end
+        context 'when current value is false' do
+          it_behaves_like "doesn't change", 'ishasmorefrags => false, islastfrag => false, isfirstfrag => false', %r{-A INPUT -p tcp -m comment --comment "599 - test"}
+        end
+        context 'when current value is true' do
+          it_behaves_like 'is idempotent', 'ishasmorefrags => true, islastfrag => true, isfirstfrag => true',
+                          %r{-A INPUT -p tcp -m frag --fragid 0 --fragmore -m frag --fragid 0 --fraglast -m frag --fragid 0 --fragfirst -m comment --comment "599 - test"}
+        end
+      end
+      context 'when set to true' do
+        before :each do
+          ip6tables_flush_all_tables
+          shell('ip6tables -A INPUT -p tcp -m frag --fragid 0 --fragmore -m frag --fragid 0 --fraglast -m frag --fragid 0 --fragfirst -m comment --comment "599 - test"')
+        end
+        context 'when current value is false' do
+          it_behaves_like 'is idempotent', 'ishasmorefrags => false, islastfrag => false, isfirstfrag => false', %r{-A INPUT -p tcp -m comment --comment "599 - test"}
+        end
+        context 'when current value is true' do
+          it_behaves_like "doesn't change", 'ishasmorefrags => true, islastfrag => true, isfirstfrag => true',
+                          %r{-A INPUT -p tcp -m frag --fragid 0 --fragmore -m frag --fragid 0 --fraglast -m frag --fragid 0 --fragfirst -m comment --comment "599 - test"}
+        end
+      end
+    end
+  end
+
   describe 'purge' do
     context 'when ipv6 chain purge', unless: os[:family] == 'redhat' && os[:release].start_with?('5') do
       after(:all) do
