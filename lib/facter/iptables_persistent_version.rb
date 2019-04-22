9aeb526748adb9f8f053d53b40e76b7c20ea6ab1
Peter Melse <pmelse-@grey2.private.feds.ca>
2019-04-22 14:02:20 -0400

    buster update

--- lib/facter/iptables_persistent_version.rb
+++ lib/facter/iptables_persistent_version.rb
@@ -6,7 +6,8 @@ Facter.add(:iptables_persistent_version) do
     os = Facter.value(:operatingsystem)
     os_release = Facter.value(:operatingsystemrelease)
     cmd = if (os == 'Debian' && (Puppet::Util::Package.versioncmp(os_release, '8.0') >= 0)) ||
-             (os == 'Ubuntu' && (Puppet::Util::Package.versioncmp(os_release, '14.10') >= 0))
+             (os == 'Ubuntu' && (Puppet::Util::Package.versioncmp(os_release, '14.10') >= 0)) ||
+             (os == 'Debian' && os_release == 'unstable')
             "dpkg-query -Wf '${Version}' netfilter-persistent 2>/dev/null"
           else
             "dpkg-query -Wf '${Version}' iptables-persistent 2>/dev/null"
