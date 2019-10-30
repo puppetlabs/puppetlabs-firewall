f797bff240e88b91bd8648ee8c41862397ea6254
Markus Opolka <markus@martialblog.de>
2019-10-30 22:18:59 +0100

    Add support for Debian Unstable
     - Added operatingsystemrelease unstable with netfilter service and package
     - Extended rspec tests for Debian

--- lib/facter/iptables_persistent_version.rb
+++ lib/facter/iptables_persistent_version.rb
@@ -6,7 +6,8 @@ Facter.add(:iptables_persistent_version) do
     os = Facter.value(:operatingsystem)
     os_release = Facter.value(:operatingsystemrelease)
     cmd = if (os == 'Debian' && (Puppet::Util::Package.versioncmp(os_release, '8.0') >= 0)) ||
-             (os == 'Ubuntu' && (Puppet::Util::Package.versioncmp(os_release, '14.10') >= 0))
+             (os == 'Ubuntu' && (Puppet::Util::Package.versioncmp(os_release, '14.10') >= 0)) ||
+             (os == 'Debian' && (Puppet::Util::Package.versioncmp(os_release, 'unstable') >= 0))
             "dpkg-query -Wf '${Version}' netfilter-persistent 2>/dev/null"
           else
             "dpkg-query -Wf '${Version}' iptables-persistent 2>/dev/null"
--- spec/unit/classes/firewall_linux_debian_spec.rb
+++ spec/unit/classes/firewall_linux_debian_spec.rb
@@ -80,4 +80,67 @@ describe 'firewall::linux::debian', type: :class do
       )
     }
   end
+
+  context 'with Debian 10' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Debian',
+        operatingsystemrelease: '10.0',
+      }
+    end
+
+    it {
+      is_expected.to contain_package('iptables-persistent').with(
+        ensure: 'present',
+      )
+    }
+    it {
+      is_expected.to contain_service('netfilter-persistent').with(
+        ensure: nil,
+        enable: 'true',
+        require: 'Package[iptables-persistent]',
+      )
+    }
+  end
+
+  context 'with Debian 10, enable => false' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Debian',
+        operatingsystemrelease: '10',
+      }
+    end
+    let(:params) { { enable: 'false' } }
+
+    it {
+      is_expected.to contain_service('netfilter-persistent').with(
+        enable: 'false',
+      )
+    }
+  end
+
+  context 'with Debian unstable' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Debian',
+        operatingsystemrelease: 'unstable',
+      }
+    end
+
+    it {
+      is_expected.to contain_package('netfilter-persistent').with(
+        ensure: 'present',
+      )
+    }
+    it {
+      is_expected.to contain_service('netfilter-persistent').with(
+        ensure: nil,
+        enable: 'true',
+        require: 'Package[netfilter-persistent]',
+      )
+    }
+  end
 end
