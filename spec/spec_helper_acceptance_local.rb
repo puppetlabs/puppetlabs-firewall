a521da2c100b9aba05b04254a8c6aa886a321427
Ciaran McCrisken <ciaran.mccrisken@puppet.com>
2020-08-05 17:13:09 +0100

    (IAC-1025) Fix spec test helper method
    Tests failing on RHEL systems as the `install_iptables` method
    always assumed it was running against Debian / Ubuntu.

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -20,7 +20,11 @@ end
 def install_iptables
   run_shell('iptables -V')
 rescue
-  run_shell('apt-get install iptables -y')
+  if os[:family] == 'redhat'
+    run_shell('yum install iptables-services -y')
+  else
+    run_shell('apt-get install iptables -y')
+  end
 end
 
 def iptables_version
