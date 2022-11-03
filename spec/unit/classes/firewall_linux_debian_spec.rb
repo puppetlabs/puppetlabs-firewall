29d7e224718189afec1611437da18b9a411cd26f
Lukas Audzevicius <laudzevicius24@hotmail.com>
2022-11-03 14:12:59 +0000

    Remove/Adjust Spec test cases

--- spec/unit/classes/firewall_linux_debian_spec.rb
+++ spec/unit/classes/firewall_linux_debian_spec.rb
@@ -3,85 +3,6 @@
 require 'spec_helper'
 
 describe 'firewall::linux::debian', type: :class do
-  context 'with Debian 8' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        operatingsystem: 'Debian',
-        operatingsystemrelease: 'jessie/sid',
-      }
-    end
-
-    it {
-      is_expected.to contain_package('iptables-persistent').with(
-        ensure: 'installed',
-      )
-    }
-    it {
-      is_expected.to contain_service('netfilter-persistent').with(
-        ensure: nil,
-        enable: 'true',
-        require: 'Package[iptables-persistent]',
-      )
-    }
-  end
-
-  context 'with deb8 enable => false' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        operatingsystem: 'Debian',
-        operatingsystemrelease: 'jessie/sid',
-      }
-    end
-    let(:params) { { enable: 'false' } }
-
-    it {
-      is_expected.to contain_service('netfilter-persistent').with(
-        enable: 'false',
-      )
-    }
-  end
-
-  context 'with Debian 8, alt operatingsystem' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        operatingsystem: 'Debian',
-        operatingsystemrelease: '8.0',
-      }
-    end
-
-    it {
-      is_expected.to contain_package('iptables-persistent').with(
-        ensure: 'installed',
-      )
-    }
-    it {
-      is_expected.to contain_service('netfilter-persistent').with(
-        ensure: nil,
-        enable: 'true',
-        require: 'Package[iptables-persistent]',
-      )
-    }
-  end
-
-  context 'with deb8, alt operatingsystem, enable => false' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        operatingsystem: 'Debian',
-        operatingsystemrelease: '8.0',
-      }
-    end
-    let(:params) { { enable: 'false' } }
-
-    it {
-      is_expected.to contain_service('netfilter-persistent').with(
-        enable: 'false',
-      )
-    }
-  end
 
   context 'with Debian 10' do
     let(:facts) do
@@ -112,6 +33,46 @@ describe 'firewall::linux::debian', type: :class do
         osfamily: 'Debian',
         operatingsystem: 'Debian',
         operatingsystemrelease: '10',
+      }
+    end
+    let(:params) { { enable: 'false' } }
+
+    it {
+      is_expected.to contain_service('netfilter-persistent').with(
+        enable: 'false',
+      )
+    }
+  end
+
+  context 'with Debian 11' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Debian',
+        operatingsystemrelease: '11.0',
+      }
+    end
+
+    it {
+      is_expected.to contain_package('iptables-persistent').with(
+        ensure: 'installed',
+      )
+    }
+    it {
+      is_expected.to contain_service('netfilter-persistent').with(
+        ensure: nil,
+        enable: 'true',
+        require: 'Package[iptables-persistent]',
+      )
+    }
+  end
+
+  context 'with Debian 11, enable => false' do
+    let(:facts) do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Debian',
+        operatingsystemrelease: '11',
       }
     end
     let(:params) { { enable: 'false' } }
--- spec/unit/puppet/provider/iptables_chain_spec.rb
+++ spec/unit/puppet/provider/iptables_chain_spec.rb
@@ -2,22 +2,12 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-if Puppet::Util::Package.versioncmp(Puppet.version, '3.4.0') < 0
-  require 'puppet/provider/confine/exists'
-else
-  require 'puppet/confine/exists'
-end
+require 'puppet/confine/exists'
 
 describe 'iptables chain' do
   describe 'iptables chain provider detection' do
-    if Puppet::Util::Package.versioncmp(Puppet.version, '3.4.0') < 0
-      let(:exists) do
-        Puppet::Provider::Confine::Exists
-      end
-    else
-      let(:exists) do
-        Puppet::Confine::Exists
-      end
+    let(:exists) do
+      Puppet::Confine::Exists
     end
 
     before :each do
