14c1371a40ae612d7e09a4e1ac7a2db0fb81f523
jordanbreen28 <jordan.breen@perforce.com>
2022-11-29 12:28:58 +0000

    (1093) - Fix unresolved fact error
    Prior to this commit, work was carried out on this module to update all instances of the now deprecated Facter::Util::Resolution, and replace all with its newer and supported counterpart Facter::Core::Execution.
    However, these do not behave exactly the same. Facter::Util::Resolution initially ran a which to locate the binary before executing, preventing any errors from occuring. The newer Facter::Core::Execution method did not do this, instead it attempted to execut>
    
    This commit aims to introduce an "on_fail:false" flag to each execute statement, so that a failed execute will return false (boolean) as oppose to an error, which can then be used for further logic.

--- spec/unit/facter/iptables_spec.rb
+++ spec/unit/facter/iptables_spec.rb
@@ -11,16 +11,20 @@ describe 'Facter::Util::Fact' do
 
   describe 'iptables_version' do
     it {
-      allow(Facter::Core::Execution).to receive(:execute).with('iptables --version')
-                                                         .and_return('iptables v1.4.7')
+      allow(Facter::Core::Execution).to receive(:which)
+        .with('iptables').and_return('/usr/sbin/iptables')
+      allow(Facter::Core::Execution).to receive(:execute)
+        .with('iptables --version', { on_fail: nil }).and_return('iptables v1.4.7')
       expect(Facter.fact(:iptables_version).value).to eql '1.4.7'
     }
   end
 
   describe 'ip6tables_version' do
     before(:each) do
+      allow(Facter::Core::Execution).to receive(:which)
+        .with('ip6tables').and_return('/usr/sbin/ip6tables')
       allow(Facter::Core::Execution).to receive(:execute)
-        .with('ip6tables --version').and_return('ip6tables v1.4.7')
+        .with('ip6tables --version', { on_fail: nil }).and_return('ip6tables v1.4.7')
     end
     it { expect(Facter.fact(:ip6tables_version).value).to eql '1.4.7' }
   end
