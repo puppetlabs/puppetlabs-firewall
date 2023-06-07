655a9d146d6f52ec30bcc338238ffb10b397daaa
david22swan <david.swan@puppet.com>
2023-06-07 17:13:03 +0100

    (CONT-242) Fix duplicate rule detection
    This was previously accomplished b retrieving the full list of rules each time a rule was set in order to check it was unique. This was to allow the user to choose the response to a duplicate rule being found.
    
    However this caused a massive slowdown within certain module runs and as such we have changed the the location of the check, so that it instead runs a check for any duplicates when retrieving the current rules prior to any updates being made.
    
    As an effect of this the user is now unable to choose the response to a duplicate rule being found, however wee feel that this is a fair tradeoff for the increased speed and that the response that we have chosen is the correct one.

--- spec/acceptance/firewall_duplicate_comment_spec.rb
+++ spec/acceptance/firewall_duplicate_comment_spec.rb
@@ -2,8 +2,19 @@
 
 require 'spec_helper_acceptance'
 
-def make_manifest(behaviour)
-  pp = <<-PUPPETCODE
+describe 'firewall - duplicate comments' do
+  before(:all) do
+    if os[:family] == 'ubuntu' || os[:family] == 'debian'
+      update_profile_file
+    end
+  end
+
+  after(:each) do
+    iptables_flush_all_tables
+  end
+
+  context 'when a duplicate comment is found' do
+    pp = <<-PUPPETCODE
     class { 'firewall': }
     resources { 'firewall':
       purge => true,
@@ -14,61 +25,15 @@ def make_manifest(behaviour)
       dport   => '550',
       action => accept,
       destination => '192.168.2.0/24',
-      onduplicaterulebehaviour => #{behaviour}
     }
     PUPPETCODE
 
-  pp
-end
-
-describe 'firewall - duplicate comments' do
-  before(:all) do
-    if os[:family] == 'ubuntu' || os[:family] == 'debian'
-      update_profile_file
-    end
-  end
-
-  before(:each) do
-    run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-  end
-
-  after(:each) do
-    iptables_flush_all_tables
-  end
-
-  context 'when onduplicateerrorhevent is set to error' do
     it 'raises an error' do
       run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-      pp = make_manifest('error')
+      run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 552 -j ACCEPT -m comment --comment "550 destination"')
 
       apply_manifest(pp) do |r|
-        expect(r.stderr).to include('Error: /Stage[main]/Main/Firewall[550 destination]: Could not evaluate: Duplicate rule found for 550 destination. Skipping update.')
-      end
-    end
-  end
-
-  context 'when onduplicateerrorhevent is set to warn' do
-    run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-
-    it 'warns and continues' do
-      run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-      pp = make_manifest('warn')
-
-      apply_manifest(pp) do |r|
-        expect(r.stderr).to include('Warning: Firewall[550 destination](provider=iptables): Duplicate rule found for 550 destination.. This may add an additional rule to the system.')
-      end
-    end
-  end
-
-  context 'when onduplicateerrorhevent is set to ignore' do
-    run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-
-    it 'continues silently' do
-      run_shell('iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 551 -j ACCEPT -m comment --comment "550 destination"')
-      pp = make_manifest('ignore')
-
-      apply_manifest(pp) do |r|
-        expect(r.stderr).to be_empty
+        expect(r.stderr).to include("Error: /Stage[main]/Main/Resources[firewall]: Failed to generate additional resources using 'generate': Duplicate rule found for 550 destination. Skipping update.")
       end
     end
   end
