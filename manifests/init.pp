6ee54a99f3cd4d5851ac9c76f552f098e07100d0
rahuls02997 <rahul.sinha@perforce.com>
2024-10-28 12:11:12 +0530

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,6 +38,7 @@ class firewall (
   Optional[String[1]]                                                $service_name_v6 = $firewall::params::service_name_v6,
   Optional[Variant[String[1], Array[String[1]]]]                     $package_name    = $firewall::params::package_name,
   Boolean                                                            $ebtables_manage = false,
+  Boolean                                                            $ebtables_manage_2 = false,
 ) inherits firewall::params {
   $_ensure_v6 = pick($ensure_v6, $ensure)
 
